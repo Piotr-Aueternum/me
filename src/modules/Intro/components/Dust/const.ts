88a9a53aad102337d79b6cece037bbf4615034b7
Piotr Sochacz <piotr.aueternum@gmail.com>
2023-09-10 23:09:17 +0200

    Adjust dust size

--- src/modules/Intro/components/Dust/const.ts
+++ src/modules/Intro/components/Dust/const.ts
@@ -2,7 +2,7 @@ import { ColorRGBA, FadingCircle } from './utils';
 
 export const MIN_SPEED = 0.08;
 export const RANGE_SPEED = 0.08;
-export const MIN_RADIUS = 10;
+export const MIN_RADIUS = 8;
 export const RADIUS_RANGE = 3;
 export const MAX_RADIUS = MIN_RADIUS + RADIUS_RANGE;
 
