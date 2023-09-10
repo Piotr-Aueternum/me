62d831d455cb84a9b725db2376f43e8412dac4fa
Piotr Sochacz <piotr.aueternum@gmail.com>
2023-09-10 23:12:58 +0200

    Added contact section

--- src/modules/Intro/Intro.tsx
+++ src/modules/Intro/Intro.tsx
@@ -7,14 +7,17 @@ export const Intro = () => {
     <>
       <div className="w-screen md:aspect-[3/1] relative flex justify-center">
         <StaticImage
-          className=" w-screen object-fill md:aspect-[3/1] md:absolute z-10"
+          className="w-screen object-fill md:aspect-[3/1] md:absolute z-10"
           src="../../images/intro.jpg"
           alt="Background image presenting cave"
         />
-        <div className="absolute z-30 top-3/4 bottom-1/4 md:bottom-auto container mx-auto">
-          <div className="text-slate-100 text-5xl">Piotr Sochacz</div>
-          <div className="text-slate-300 text-2xl">Software Engineer</div>
-        </div>
+        <h1 className="absolute z-30 top-3/4 bottom-1/4 md:bottom-auto container mx-auto">
+          <span className="text-slate-100 text-5xl">Piotr Sochacz</span>
+          <br />
+          <span className="text-slate-300 text-2xl  font-serif">
+            Software Engineer
+          </span>
+        </h1>
         <Dust className="w-screen md:aspect-[3/1] absolute z-20" />
       </div>
     </>
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,1 +1,2 @@
 export { Intro } from './Intro/Intro';
+export { Contact } from './Contact/Contact';
